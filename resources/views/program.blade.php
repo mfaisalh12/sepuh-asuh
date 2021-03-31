b1fefc33ef81c922f062b254ffdc9cb98ea20b11
Faisal <mfaisalhidayatullah@gmail.com>
2021-03-31 19:30:57 +0700

    menghubungkan view program ke database program

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -64,11 +64,12 @@
     <!-- card start -->
     <div class="jumbotron">
         <div class="row">
+        @foreach($program as $program)
             <div class="col-lg">
                 <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;;">
-                    <img src="../img/gambar1.jpg" class="card-img-top" alt="...">
+                    <img src="{{ url('uploads') }}/{{ $program->img_program }}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b> Bantu Ayah Difabel Agar Tidak Melaut Lagi</b></h5>
+                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b>{{ $program->berita_program }}</b></h5>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">
@@ -76,98 +77,12 @@
                                 <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                             <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. 49,350,000</div>
+                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. {{ number_format($program->jumlah_donasi) }}</div>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="col-lg">
-                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;">
-                    <img src="../img/gambar1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b> Bantu Ayah Difabel Agar Tidak Melaut Lagi</b></h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. 49,350,000</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg">
-                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;">
-                    <img src="../img/gambar1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b> Bantu Ayah Difabel Agar Tidak Melaut Lagi</b></h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. 49,350,000</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg">
-                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;">
-                    <img src="../img/gambar1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b> Bantu Ayah Difabel Agar Tidak Melaut Lagi</b></h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. 49,350,000</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg">
-                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;">
-                    <img src="../img/gambar1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b> Bantu Ayah Difabel Agar Tidak Melaut Lagi</b></h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. 49,350,000</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg">
-                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;">
-                    <img src="../img/gambar1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b> Bantu Ayah Difabel Agar Tidak Melaut Lagi</b></h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. 49,350,000</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        @endforeach
         </div>
     </div>
 
