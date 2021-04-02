25dc540793f03f14de7b2096cdc8bbf340549b00
Faisal <mfaisalhidayatullah@gmail.com>
2021-04-02 11:39:45 +0700

    memperbaiki tampilan program

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -8,31 +8,31 @@
 
 
     <!-- card start -->
-    <div class="jumbotron">
-        <div class="row">
-        @foreach($program as $item)
-            <div class="col-lg">
-                <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;;">
-                    <img src="{{ url('uploads') }}/{{ $item->img_program }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b>{{ $item->berita_program }}</b></h5>
+    <div class="card-container">
+        <div>
+            <div class="row">
+            @foreach($program as $item)
+                <div class="col-lg-4">
+                    <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;;">
+                        <img src="{{ url('uploads') }}/{{ $item->img_program }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b>{{ $item->berita_program }}</b></h5>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
+                                <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. {{ number_format($item->jumlah_donasi) }}</div>
+                            </li>
+                        </ul>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. {{ number_format($item->jumlah_donasi) }}</div>
-                        </li>
-                    </ul>
-                </div>
-            </div>       
-        @endforeach
+                </div>       
+            @endforeach
+            </div>
         </div>
-    </div>
-    <div class="container position-relative end-0">
-        <div class="position-absolute top-0 end-0">
+        <div class="container">
             {{$program->links()}}
         </div>
     </div>
