504ae079f2f63f4c4b759496026cf33aeb28f728
Faisal <mfaisalhidayatullah@gmail.com>
2021-04-02 07:46:23 +0700

    membuat pagination pada page  program

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -10,12 +10,12 @@
     <!-- card start -->
     <div class="jumbotron">
         <div class="row">
-        @foreach($program as $program)
+        @foreach($program as $item)
             <div class="col-lg">
                 <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 348px;;">
-                    <img src="{{ url('uploads') }}/{{ $program->img_program }}" class="card-img-top" alt="...">
+                    <img src="{{ url('uploads') }}/{{ $item->img_program }}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b>{{ $program->berita_program }}</b></h5>
+                        <h5 class="card-title text-start" style="font-family: poppins, sans-serif; font-size: 20px;"><b>{{ $item->berita_program }}</b></h5>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">
@@ -23,12 +23,18 @@
                                 <div class="progress-bar" role="progressbar" style="width: 65%; background-color: #1993C8; border-radius: 20px;;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                             <div class="char-name" style="opacity: 60%; font-family: poppins,sans-serif; font-size: 12px;">Terkumpul</div>
-                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. {{ number_format($program->jumlah_donasi) }}</div>
+                            <div class="char-name" style="font-weight: bold; font-family: poppins, sans-serif; font-size: 14px;">Rp. {{ number_format($item->jumlah_donasi) }}</div>
                         </li>
                     </ul>
                 </div>
-            </div>
+            </div>       
         @endforeach
         </div>
     </div>
+    <div class="container position-relative end-0">
+        <div class="position-absolute top-0 end-0">
+            {{$program->links()}}
+        </div>
+    </div>
+
 @endsection
