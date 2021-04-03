4e8c8a0363547bca5b974aa4396f2c1a1d334422
Faisal <mfaisalhidayatullah@gmail.com>
2021-04-03 11:41:35 +0700

    menambahkan style di donasi page

--- resources/views/donasi.blade.php
+++ resources/views/donasi.blade.php
@@ -1,61 +1,62 @@
 @extends('layout.app')
 
 @section('style')
-    <link rel="stylesheet" href="{{ asset('css/index.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/donasi.css')}}">
 @endsection
 
 @section('content')
-    <h1>DONASI</h1>
-    <a href="/">
-      <button class="btn btn-primary" type="submit">Back</button></a>
+    <div class="container-donasi">
+        <h1>DONASI</h1>
+        <a href="/">
+        <button class="btn btn-primary" type="submit">Back</button></a>
 
-      @if ($errors->any())
-      <div class="alert alert-danger">
-          <ul>
-              @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-              @endforeach
-          </ul>
-      </div>
-    @endif
-    
-    <form action="/donasi" class="row g-3" method="POST">
-        @csrf
-        <div class="col-12">
-          <label for="inputNama" class="form-label">Nama</label>
-          <input type="text" class="form-control" id="inputName">
-
-          
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-        <div class="col-12">
-          <label for="inputDonasi" class="form-label">Jumlah Donasi</label>
-          <input type="text" class="form-control" id="inputDonasi" name="jumlah_donasi">
-          <?$jumlah_donasi = $_POST["jumlah_donasi"];?>
-        </div>
+        @endif
         
-          <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-            <option selected>Pilih Program yang akan di donasi</option>
-            <option value="1">Modal Usaha</option>
-            <option value="2">Biaya Hidup</option>
-            <option value="3">Biaya Pendidikan</option>
-          </select>
-          
-          <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-            <option selected>Pilih Orang yang akan di donasi</option>
-              <option value="1">Orang 1</option>
-              <option value="2">Orang 2</option>
-              <option value="3">Orang 3</option>
-          </select>
-          <div class="mb-3"><div class="mb-3">
-            <label for="formFile" class="form-label">Bukti Pembayaran</label>
-            <b><br>No. Rekening : 32144123XXXXXXXX (BANK A)<br>No. Rekening : 32413412XXXXXXXX (BANK B)</b>
-            <input class="form-control" type="file" id="formFile" name="bukti_donasi">
-            <?$bukti_donasi = $_POST["bukti_donasi"];?>
-          </div>
-         </div>
-        <div class="col-12">
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-      </form>
+        <form action="/donasi" class="row g-3" method="POST">
+            @csrf
+            <div class="col-12">
+                <label for="inputNama" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="inputName">
+            </div>
+            <div class="col-12">
+                <label for="inputDonasi" class="form-label">Jumlah Donasi</label>
+                <input type="text" class="form-control" id="inputDonasi" name="jumlah_donasi">
+                <?$jumlah_donasi = $_POST["jumlah_donasi"];?>
+            </div>
+            
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                <option selected>Pilih Program yang akan di donasi</option>
+                <option value="1">Modal Usaha</option>
+                <option value="2">Biaya Hidup</option>
+                <option value="3">Biaya Pendidikan</option>
+            </select>
+            
+            <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                <option selected>Pilih Orang yang akan di donasi</option>
+                <option value="1">Orang 1</option>
+                <option value="2">Orang 2</option>
+                <option value="3">Orang 3</option>
+            </select>
+            <div class="mb-3"><div class="mb-3">
+                <label for="formFile" class="form-label">Bukti Pembayaran</label>
+                <b><br>No. Rekening : 32144123XXXXXXXX (BANK A)<br>No. Rekening : 32413412XXXXXXXX (BANK B)</b>
+                <input class="form-control" type="file" id="formFile" name="bukti_donasi">
+                <?$bukti_donasi = $_POST["bukti_donasi"];?>
+            </div>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
+
     <!-- end card -->
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,17 +47,16 @@
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+        </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+        </button>
     </div>
 
     <!-- carousel slide end -->
-
     <div class="heading-fluid mt-5">
         <h1 class="text-center blue-title">BERITA TERBARU</h1>
     </div>
@@ -80,6 +79,8 @@
         @endforeach
         </div>
     </div>
+
+
 
     <!-- end card -->
 @endsection
